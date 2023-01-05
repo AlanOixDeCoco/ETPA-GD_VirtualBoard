8cd38bf26017f2fc4509e16a5a5cc78a3bd174c6
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-01-05 10:43:43 +0100

    Create javascript code

--- source.js
+++ source.js
@@ -1,5 +1,41 @@
-playerMoneyList = document.getElementsByClassName("playerMoney");
-playerMoneyList.forEach(playerMoney => {
-    id = playerMoney.id;
-    
+const ADD_AMOUNT = 1;
+const BUY_AMOUNT = 2;
+const SELL_AMOUNT = 1;
+
+let playersCurrency = [2, 2, 2, 2];
+
+let buttonsContainerList = document.getElementsByClassName("buttonsContainer");
+let playerIndex = 0;
+Array.from(buttonsContainerList).forEach((buttonsContainer) => {
+    let index = playerIndex;
+    buttonsContainer.getElementsByClassName("button_add")[0].onclick = () => {
+        AddCurrency(index, ADD_AMOUNT);
+    };
+    buttonsContainer.getElementsByClassName("button_buy")[0].onclick = () => {
+        RemoveCurrency(index, BUY_AMOUNT);
+    };
+    buttonsContainer.getElementsByClassName("button_sell")[0].onclick = () => {
+        AddCurrency(index, SELL_AMOUNT);
+    };
+
+    RefreshCurrency(playerIndex);
+
+    playerIndex++;
 });
+
+function AddCurrency(playerIndex, amount){
+    playersCurrency[playerIndex] += amount;
+    
+    RefreshCurrency(playerIndex);
+}
+
+function RemoveCurrency(playerIndex, amount){
+    playersCurrency[playerIndex] -= amount;
+    playersCurrency[playerIndex] < 0 && (playersCurrency[playerIndex] = 0);
+
+    RefreshCurrency(playerIndex);
+}
+
+function RefreshCurrency(playerIndex){
+    document.getElementById(`player${playerIndex+1}_currency`).innerHTML = playersCurrency[playerIndex];
+}
